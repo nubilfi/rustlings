6193d1163ecfd12ad65fd291bbcb3a46d29dfbaf
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2023-03-20 15:17:35 +0700

    Initial commit

---
+++ exercises/hashmaps/hashmaps1.rs
@@ -0,0 +1,45 @@
+// hashmaps1.rs
+// A basket of fruits in the form of a hash map needs to be defined.
+// The key represents the name of the fruit and the value represents
+// how many of that particular fruit is in the basket. You have to put
+// at least three different types of fruits (e.g apple, banana, mango)
+// in the basket and the total count of all the fruits should be at
+// least five.
+//
+// Make me compile and pass the tests!
+//
+// Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
+
+// I AM DONE
+
+use std::collections::HashMap;
+
+fn fruit_basket() -> HashMap<String, u32> {
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
+
+    // Two bananas are already given for you :)
+    basket.insert(String::from("banana"), 2);
+
+    // TODO: Put more fruits in your basket here.
+    basket.entry(String::from("apple")).or_insert(1);
+    basket.entry(String::from("mango")).or_insert(2);
+
+    basket
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn at_least_three_types_of_fruits() {
+        let basket = fruit_basket();
+        assert!(basket.len() >= 3);
+    }
+
+    #[test]
+    fn at_least_five_fruits() {
+        let basket = fruit_basket();
+        assert!(basket.values().sum::<u32>() >= 5);
+    }
+}
---
+++ exercises/variables/variables3.rs
@@ -0,0 +1,9 @@
+// variables3.rs
+// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a hint.
+
+// I AM DONE
+
+fn main() {
+    let mut x: i32 = 3;
+    println!("Number {}", x);
+}
